8cebaaacfa5227a2c50be9ccbf0b10752bc82ea2
Mutuh <sirmutuh@gmail.com>
2013-05-31 14:40:30 +0300

    small update

--- battle-city-v3/js/multiplayer.js
+++ battle-city-v3/js/multiplayer.js
@@ -1,7 +1,25 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: mutuh
- * Date: 5/21/13
- * Time: 11:16 AM
- * To change this template use File | Settings | File Templates.
- */
+var multiplayer = {
+    // Open multiplayer game lobby
+    websocket_url:"ws://localhost:8080/",
+    websocket:undefined,
+    start:function(){
+        var WebSocketObject = window.WebSocket || window.MozWebSocket;
+        if (!WebSocketObject){
+            console.log("Your browser does not support WebSocket. Multiplayer will not work.");
+            return;
+        }
+        this.websocket = new WebSocketObject(this.websocket_url);
+
+        this.websocket.onopen = function(){
+            console.log('open');
+        }
+
+        this.websocket.onclose = function(){
+            console.log('close');
+        }
+
+        this.websocket.onerror = function(){
+            console.log('error');
+        }
+    }
+}
--- battle-city-v3/js/server.js
+++ battle-city-v3/js/server.js
@@ -1,7 +1,35 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: mutuh
- * Date: 5/21/13
- * Time: 11:15 AM
- * To change this template use File | Settings | File Templates.
- */
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+// Create a simple web server that returns the same response for any request
+var server = http.createServer(function(request,response){
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end("This is the node.js HTTP server.");
+});
+
+server.listen(8080,function(){
+    console.log('Server has started listening on port 8080');
+});
+
+var wsServer = new WebSocketServer({
+    httpServer:server,
+    autoAcceptConnections: false
+});
+
+// Logic to determine whether a specified connection is allowed.
+function connectionIsAllowed(request){
+    // Check criteria such as request.origin, request.remoteAddress
+    return true;
+}
+
+wsServer.on('request',function(request){
+    if(!connectionIsAllowed(request)){
+        request.reject();
+        console.log('Connection from ' + request.remoteAddress + ' rejected.');
+        return;
+    }
+
+    var connection = request.accept();
+    console.log('Connection from ' + request.remoteAddress + ' accepted.');
+
+});
